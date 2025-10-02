96a0d4fe0b70ba346a00173088d1b57659c41cdd
rhuanlunz <rhuan@C0V1L>
2025-10-02 19:38:15 -0300

    Added database

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from infrastructure.database import initialize_database
 from routes.dog_api import dog_api_blueprint
 from routes.menu import menu_blueprint
 from routes.game import game_blueprint
@@ -8,6 +9,8 @@ app = Flask(__name__)
 app.register_blueprint(dog_api_blueprint, url_prefix="/api/dog")
 app.register_blueprint(menu_blueprint, url_prefix="/menu")
 app.register_blueprint(game_blueprint, url_prefix="/game")
+
+initialize_database()
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ src/infrastructure/database.py
@@ -0,0 +1,37 @@
+import sqlite3
+
+DATABASE_PATH = "./database.db"
+
+def initialize_database():
+    with sqlite3.connect(DATABASE_PATH) as connection:
+        cursor = connection.cursor()
+        cursor.execute("""CREATE TABLE IF NOT EXISTS games(
+                       uuid VARCHAR(36) PRIMARY KEY,
+                       dog_name VARCHAR(255) NOT NULL DEFAULT '',
+                       dog_bread VARCHAR(255) NOT NULL DEFAULT '',
+                       dog_hunger INTEGER NOT NULL DEFAULT 0,
+                       dog_fatigue INTEGER NOT NULL DEFAULT 0,
+                       dog_is_sleeping BOOLEAN NOT NULL DEFAULT TRUE,
+                       dog_is_dead BOOLEAN NOT NULL DEFAULT FALSE)""")
+        
+        connection.commit()
+
+def create_game(game_uuid: str) -> None:
+    with sqlite3.connect(DATABASE_PATH) as connection:
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO games(uuid) VALUES(?)", (game_uuid,))
+        connection.commit()
+
+def find_game_by_uuid(game_uuid: str):
+    with sqlite3.connect(DATABASE_PATH) as connection:
+        cursor = connection.cursor()
+        game = cursor.execute("SELECT * FROM games WHERE uuid = ?", (game_uuid,)).fetchone()
+    
+    return game
+
+def is_game_exist(game_uuid: str) -> bool:
+    with sqlite3.connect(DATABASE_PATH) as connection:
+        cursor = connection.cursor()
+        game = cursor.execute("SELECT * FROM games WHERE uuid = ?", (game_uuid,)).fetchone()
+    
+    return game is not None
